fec647ff8bca65ba43aa094ff1b010e510740523
Fewdy <dipzei.vibes@gmail.com>
2023-02-07 04:07:36 -0500

    Updated Tags

--- FewTags/FewTags/Main.cs
+++ FewTags/FewTags/Main.cs
@@ -158,6 +158,9 @@ namespace FewTags
                 s_textMeshProGmj2.GetComponent<TMPro.TextMeshProUGUI>().autoSizeTextContainer = true;
                 s_textMeshProGmj2.GetComponent<TMPro.TextMeshProUGUI>().color = new Color(r, g, b, 0.55f);
                 s_textMeshProGmj2.transform.localScale = new Vector3(0.03f, 0.03f, 0.03f);
+
+                s_dev = GameObject.Find("/" + uid + "[NamePlate]/Canvas/Content/Disable with Menu").gameObject.GetComponent<RectTransform>().gameObject;
+                s_dev.transform.gameObject.SetActive(false);
             }
             catch { }
         }
