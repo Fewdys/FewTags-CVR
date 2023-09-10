68d0ce1aa8389636b53ad647dfad83880eb8850a
ShoutaDev <dev.shouta@gmail.com>
2023-09-09 23:27:08 -0400

    Improved multiple parts of code and fixed issues in which Fewdy wasn't even aware of

--- FewTags/FewTags/Main.cs
+++ FewTags/FewTags/Main.cs
@@ -1,22 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using ABI_RC.Core.Player;
+using ABI_RC.Core.UI;
+using FewTags.Utils;
+using Harmony;
 using MelonLoader;
 using Newtonsoft.Json;
-using System.IO;
-using FewTags.Utils;
-using System.Net;
-using ABI_RC.Core.Player;
-using ABI_RC.Core.UI;
-using ABI_RC.Core;
-using Harmony;
-using UnityEngine;
+using System;
 using System.Collections;
-using ABI_RC.Core.Base.Jobs;
-using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using UnityEngine;
 
 // Thanks To Edward7 For The Original Base
 
@@ -24,7 +18,7 @@ namespace FewTags
 {
     public class Main : MelonMod
     {
-        public static bool NocturnalTagsLoaded { get; private set; }
+        public static bool ChatBoxLoaded { get; private set; }
 
         private static float a;
         private static float b;
@@ -45,11 +39,11 @@ namespace FewTags
 
         public override void OnApplicationStart()
         {
-            NocturnalTagsLoaded = MelonHandler.Mods.Any(m => m.Info.Name == "Nocturnal Plates");
+            ChatBoxLoaded = MelonHandler.Mods.Any(m => m.Info.Name == "ChatBox");
             MelonLogger.Msg("Initializing.");
             MelonLogger.Msg("FewTags Loaded. Press Slash To Reload Tags");
             DownloadString();
-            _hInstance.Patch(typeof(PlayerNameplate).GetMethod(nameof(PlayerNameplate.UpdateNamePlate)), null, typeof(Main).GetMethod(nameof(OnPlayerJoin),System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static).ToNewHarmonyMethod());
+            _hInstance.Patch(typeof(PlayerNameplate).GetMethod(nameof(PlayerNameplate.UpdateNamePlate)), null, typeof(Main).GetMethod(nameof(OnPlayerJoin), System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static).ToNewHarmonyMethod());
             MelonCoroutines.Start(WaitForNamePlate());
         }
 
@@ -60,7 +54,7 @@ namespace FewTags
             s_namePlate = Resources.FindObjectsOfTypeAll<PuppetMaster>().FirstOrDefault(x => x.name == "_NetworkedPlayerObject").transform.Find("[NamePlate]/Canvas/Content").gameObject;
         }
 
-        // Keybind To Update The Tags (Prevents The Need Of Needing To Restart You're Game)
+        // Keybind To Re-Fetch The Tags (Prevents The Need Of Having To Restart You're Game)
         public override void OnUpdate()
         {
             if (Input.GetKeyDown(KeyCode.Slash))
@@ -78,7 +72,10 @@ namespace FewTags
             {
                 _userArr.Clear();
             }
-            catch { }
+            catch // not gonna let yourself know if there was an issue clearing?? let me fix that for you
+            {
+                MelonLogger.Msg("Couldn't Clear Tags List - Probably Because There Were No Tags To Clear");
+            }
             DownloadString();
         }
 
@@ -90,17 +87,27 @@ namespace FewTags
             s_uId = __instance.transform.parent.name;
             s_user = _userArr.FirstOrDefault(x => x.UserId == s_uId);
             if (s_user == null) return;
-            for (int i = 0; i < s_user.NamePlatesText.Length; i++)
-                GeneratePlate(s_uId, s_user.NamePlatesText[i], i, new Color32(byte.Parse(s_user.Color[0].ToString()), byte.Parse(s_user.Color[1].ToString()), byte.Parse(s_user.Color[2].ToString()), byte.Parse(s_user.Color[3].ToString())));
-            for (int i = 0; i < s_user.BigPlatesText.Length; i++)
-                GenerateBigPlate(s_uId, s_user.BigPlatesText[i], i);
+            if (GameObject.Find($"{s_uId}/[NamePlate]/Canvas/FewTags-NamePlate") == null) // why wasnt there a check for if it already exists?
+            {
+                for (int i = 0; i < s_user.NamePlatesText.Length; i++)
+                {
+                    GeneratePlate(s_uId, s_user.NamePlatesText[i], i, new Color32(byte.Parse(s_user.Color[0].ToString()), byte.Parse(s_user.Color[1].ToString()), byte.Parse(s_user.Color[2].ToString()), byte.Parse(s_user.Color[3].ToString())));
+                }
+            }
+            if (GameObject.Find($"{s_uId}/[NamePlate]/Canvas/FewTags-BigNamePlate") == null) // again why wasnt there a check for if it already exists?
+            {
+                for (int i = 0; i < s_user.BigPlatesText.Length; i++)
+                {
+                    GenerateBigPlate(s_uId, s_user.BigPlatesText[i], i);
+                }
+            }
             /*CreateLogo(s_uId);*/
         }
 
         private static float s_textCount { get; set; }
         private static GameObject s_imageHolder { get; set; }
 
-        private static void GeneratePlate(string uid, string plateText, int multiplier,Color32 color)
+        private static void GeneratePlate(string uid, string plateText, int multiplier, Color32 color)
         {
             // This Was Used For Testing Mainly To Check Lengths Of Things (Sorta Math Related I Guess)
             // MelonLogger.Msg("---PlateText---");
@@ -108,62 +115,99 @@ namespace FewTags
             // MelonLogger.Msg("---PlateText Length---");
             // MelonLogger.Msg(plateText.Length);
 
-            try  // Try Catch For Incase The Tag Somehow Manages To Mess Up
+            if (plateText == null) { return; } // why make the plate if it has no text to start with?
+            else
             {
-                s_textCount = plateText.Contains("<color=") ? plateText.Length - (Regex.Matches(plateText, "<color=").Count != 1 ? Regex.Matches(plateText, "<color=").Count * 23 - 3 : 20) : plateText.Length;
-                s_MainPlateHolder = GameObject.Instantiate(s_namePlate, GameObject.Find("/" + uid + "[NamePlate]/Canvas").transform);
-                s_MainPlateHolder.transform.localPosition = new Vector3(0, -0.155f - (multiplier) * 0.0778f, 0);
-                s_imageHolder = s_MainPlateHolder.transform.Find("Image").gameObject;
-                s_imageHolder.GetComponent<UnityEngine.UI.Image>().color = color;
-                GameObject.Destroy(s_MainPlateHolder.transform.Find("Image/FriendsIndicator").gameObject);
-                GameObject.Destroy(s_MainPlateHolder.transform.Find("Image/ObjectMaskSlave").gameObject);
-                GameObject.Destroy(s_MainPlateHolder.transform.Find("Disable with Menu").gameObject);
-                s_MainPlateHolder.transform.localScale = new Vector3(0.3f, 0.3f, 1);
-                s_imageHolder.transform.localScale = new Vector3(1, 0.5f, 1);
-                s_imageHolder.GetComponent<RectTransform>().sizeDelta = new Vector2(s_textCount / 10, 0.5f);
-                s_textMeshProGmj = s_MainPlateHolder.transform.Find("TMP:Username").gameObject;
-                s_textMeshProGmj.transform.localScale = new Vector3(0.58f, 0.58f, 1);
-                s_textMeshProGmj.transform.localPosition = Vector3.zero;
-                s_textMeshProGmj.GetComponent<TMPro.TextMeshProUGUI>().text = plateText;
-                s_textMeshProGmj.GetComponent<TMPro.TextMeshProUGUI>().alignment = TMPro.TextAlignmentOptions.Center;
-                s_textMeshProGmj.GetComponent<TMPro.TextMeshProUGUI>().autoSizeTextContainer = true;
-                s_textMeshProGmj.gameObject.GetComponent<UnityEngine.RectTransform>().anchoredPosition = new Vector2(-0.05f, 0f);
+                try  // Try Catch For Incase The Tag Somehow Manages To Mess Up -- Improved For You <3
+                {
+                    s_textCount = plateText.Contains("<color=") ? plateText.Length - (Regex.Matches(plateText, "<color=").Count != 1 ? Regex.Matches(plateText, "<color=").Count * 23 - 3 : 20) : plateText.Length;
+                    s_MainPlateHolder = GameObject.Instantiate(s_namePlate, GameObject.Find("/" + uid + "[NamePlate]/Canvas").transform);
+                    s_MainPlateHolder.transform.localPosition = new Vector3(0, -0.155f - (multiplier) * 0.0778f, 0);
+                    s_MainPlateHolder.name = "FewTags-NamePlate"; // why were you not naming the object to be able to find it if needed?
+                    s_MainPlateHolder.layer = 69; // ;)
+                    s_imageHolder = s_MainPlateHolder.transform.Find("Image").gameObject;
+                    s_imageHolder.GetComponent<UnityEngine.UI.Image>().color = color;
+                    GameObject.Destroy(s_MainPlateHolder.transform.Find("Image/FriendsIndicator").gameObject);
+                    GameObject.Destroy(s_MainPlateHolder.transform.Find("Image/ObjectMaskSlave").gameObject);
+                    GameObject.Destroy(s_MainPlateHolder.transform.Find("Disable with Menu").gameObject);
+                    s_MainPlateHolder.transform.localScale = new Vector3(0.3f, 0.3f, 1);
+                    s_imageHolder.transform.localScale = new Vector3(1, 0.5f, 1);
+                    s_imageHolder.GetComponent<RectTransform>().sizeDelta = new Vector2(s_textCount / 10, 0.5f);
+                    s_textMeshProGmj = s_MainPlateHolder.transform.Find("TMP:Username").gameObject;
+                    s_textMeshProGmj.transform.localScale = new Vector3(0.58f, 0.58f, 1);
+                    s_textMeshProGmj.transform.localPosition = Vector3.zero;
+                    s_textMeshProGmj.gameObject.GetComponent<UnityEngine.RectTransform>().anchoredPosition = new Vector2(-0.05f, 0f);
+                    var tmpc = s_textMeshProGmj.GetComponent<TMPro.TextMeshProUGUI>(); // why make life more diffucult and not just do this?
+                    tmpc.text = plateText;
+                    tmpc.alignment = TMPro.TextAlignmentOptions.Center;
+                    tmpc.autoSizeTextContainer = true;
+                    tmpc.enableCulling = true;
+                    tmpc.material.enableInstancing = true;
 
 
-                // Done Just For Removing The Text Under Devs/Mods - Doesn't Effect Being Able To See Who Is A Dev/Mod ect. (Done For Personal Preference To Make Things Cleaner)
-                s_dev = GameObject.Find("/" + uid + "[NamePlate]/Canvas/Content/Disable with Menu").gameObject.GetComponent<RectTransform>().gameObject;
-                s_dev.transform.gameObject.SetActive(false);
+                    // Done Just For Removing The Text Under Devs/Mods - Doesn't Effect Being Able To See Who Is A Dev/Mod ect. (Done For Personal Preference To Make Things Cleaner)
+                    s_dev = GameObject.Find("/" + uid + "[NamePlate]/Canvas/Content/Disable with Menu").gameObject.GetComponent<RectTransform>().gameObject;
+                    s_dev.transform.gameObject.SetActive(false);
+                }
+                catch // why not have it tell us where something went wrong instead of just basically catching it each time it fails
+                {
+                    if (uid != null)
+                    {
+                        MelonLogger.Msg($"Failed To Create Nameplate On {uid}");
+                    }
+                    else
+                    {
+                        MelonLogger.Msg($"Failed To Create Nameplate");
+                    }
+                }
             }
-            catch { }
         }
 
-        // Duplicated GeneratePlate And Changed/Added A Bit Because I Was Lazy And Wanted A Specific Spot For Big Text
+        // Duplicated GeneratePlate And Changed/Added A Bit Because I Was Lazy And Wanted A Specific Spot For Big Text -- You're just like me fr stop being lazy tho :p
         private static void GenerateBigPlate(string uid, string plateText, int multiplier)
         {
-            try  // Try Catch For Incase The Tag Somehow Manages To Mess Up
+            if (plateText == null) { return; } // again why make the plate if it has no text to start with?
+            else
             {
-                s_BigPlateHolder = GameObject.Instantiate(s_namePlate, GameObject.Find("/" + uid + "[NamePlate]/Canvas").transform);
-                string[] splited = plateText.Split(new string[] { "<size=" }, StringSplitOptions.None);
-                string sizeString = string.Empty;
-                for (int i = 0; i < splited[1].Length; i++)
+                try  // Try Catch For Incase The Tag Somehow Manages To Mess Up -- Improved For You <3
                 {
-                    if (!char.IsDigit(splited[1][i])) break;
-                    sizeString += splited[1][i];
+                    s_BigPlateHolder = GameObject.Instantiate(s_namePlate, GameObject.Find("/" + uid + "[NamePlate]/Canvas").transform);
+                    s_BigPlateHolder.name = "FewTags-BigNamePlate"; // again why were you not naming the object to be able to find it if needed?
+                    s_BigPlateHolder.layer = 69; // ;)
+                    string[] splited = plateText.Split(new string[] { "<size=" }, StringSplitOptions.None);
+                    string sizeString = string.Empty;
+                    for (int i = 0; i < splited[1].Length; i++)
+                    {
+                        if (!char.IsDigit(splited[1][i])) break;
+                        sizeString += splited[1][i];
+                    }
+                    // Moves Big Text Based On Weather Or Not ChatBox Is Loaded -- Idk Why You Were Checking For A Deprecated Mod
+                    s_BigPlateHolder.transform.localPosition = ChatBoxLoaded ? new Vector3(0, 0.45f + (int.Parse(sizeString)) * 0.0075f, 0) : new Vector3(0, 0.45f + (int.Parse(sizeString)) * 0.0035f, 0);
+                    GameObject.Destroy(s_BigPlateHolder.transform.Find("Image").gameObject.GetComponent<UnityEngine.UI.Image>());
+                    GameObject.Destroy(s_BigPlateHolder.transform.Find("Image/FriendsIndicator").gameObject);
+                    GameObject.Destroy(s_BigPlateHolder.transform.Find("Image/ObjectMaskSlave").gameObject);
+                    GameObject.Destroy(s_BigPlateHolder.transform.Find("Disable with Menu").gameObject);
+                    s_textMeshProGmj2 = s_BigPlateHolder.transform.Find("TMP:Username").gameObject;
+                    s_textMeshProGmj2.transform.localPosition = Vector3.zero;
+                    s_textMeshProGmj2.transform.localScale = new Vector3(0.03f, 0.03f, 0.03f);
+                    var bptmpc = s_textMeshProGmj2.GetComponent<TMPro.TextMeshProUGUI>(); // again why make life more diffucult and not just do this?
+                    bptmpc.text = plateText;
+                    bptmpc.autoSizeTextContainer = true;
+                    bptmpc.color = new Color(r, g, b, 0.55f);
+                    bptmpc.enableCulling = true;
                 }
-                // Moves Big Text Based On Weather Or Not Nocturnal Tags Is Loaded
-                s_BigPlateHolder.transform.localPosition = NocturnalTagsLoaded ? new Vector3(0, 0.758f + (int.Parse(sizeString)) * 0.0075f, 0) : new Vector3(0, 0.45f + (int.Parse(sizeString)) * 0.0035f, 0);
-                GameObject.Destroy(s_BigPlateHolder.transform.Find("Image").gameObject.GetComponent<UnityEngine.UI.Image>());
-                GameObject.Destroy(s_BigPlateHolder.transform.Find("Image/FriendsIndicator").gameObject);
-                GameObject.Destroy(s_BigPlateHolder.transform.Find("Image/ObjectMaskSlave").gameObject);
-                GameObject.Destroy(s_BigPlateHolder.transform.Find("Disable with Menu").gameObject);
-                s_textMeshProGmj2 = s_BigPlateHolder.transform.Find("TMP:Username").gameObject;
-                s_textMeshProGmj2.transform.localPosition = Vector3.zero;
-                s_textMeshProGmj2.GetComponent<TMPro.TextMeshProUGUI>().text = plateText;
-                s_textMeshProGmj2.GetComponent<TMPro.TextMeshProUGUI>().autoSizeTextContainer = true;
-                s_textMeshProGmj2.GetComponent<TMPro.TextMeshProUGUI>().color = new Color(r, g, b, 0.55f);
-                s_textMeshProGmj2.transform.localScale = new Vector3(0.03f, 0.03f, 0.03f);
+                catch // again why not have it tell us where something went wrong instead of just basically catching it each time it fails
+                {
+                    if (uid != null)
+                    {
+                        MelonLogger.Msg($"Failed To Create BigNameplate On {uid}");
+                    }
+                    else
+                    {
+                        MelonLogger.Msg($"Failed To Create BigNameplate");
+                    }
+                }
             }
-            catch { }
         }
 
         // At Some Point Will Make This Show Up For Each Person Running The Mod If I Figure Out How To Since I Don't Have A Server Nor Do I Plan On Using One
@@ -192,7 +236,7 @@ namespace FewTags
                     MelonLogger.Msg(ConsoleColor.Green, "Downloaded Tags");
                 }
             }
-            catch { MelonLogger.Msg(ConsoleColor.DarkRed, "Error Downloading Tags (Likely A Github Issue or A Internet/Service Issue)"); }
+            catch { MelonLogger.Msg(ConsoleColor.DarkRed, "Error Downloading Tags (Likely A Github Issue or A Internet/Service Issue)"); } // see like this you gave a reason for a possible issue. you should do that way more often when doing try catch
 
         }
     }
